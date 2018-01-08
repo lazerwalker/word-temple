92821f40bbe31cbd8bd120eac4e40001951c7cbf
Mike Lazer-Walker <michael@lazerwalker.com>
2018-01-08 17:59:44 -0500

    Add some garbage to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 import BoardView from './components/BoardView';
 import RackView from './components/RackView';
 
+import { checkWord } from './Board';
+
 import { Tile } from './Tile';
 import TileBag from './TileBag';
 
@@ -14,10 +16,14 @@ class App extends React.Component {
     [hand, bag] = bag.drawHand();
 
     const tiles = [
+      {x: 0, y: 1, letter: 'R', value: 3},
       {x: 1, y: 1, letter: 'A', value: 1},
+      {x: 2, y: 1, letter: 'T', value: 1},
       {x: 3, y: 5, letter: 'Z', value: 10}
     ];
 
+    const cat = [tiles[0], tiles[1], tiles[2]];
+    console.log(checkWord(cat));
     return (
       <div className="App">
         <BoardView
