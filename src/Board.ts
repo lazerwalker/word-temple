ec1ab8b556fa0313ab4d44d851b923577d956d07
Mike Lazer-Walker <michael@lazerwalker.com>
2018-02-11 00:46:06 +0100

    Fix issue with clients swapping board tiles

--- src/Board.ts
+++ src/Board.ts
@@ -105,7 +105,7 @@ export function boardByAddingTile(
 
 export function boardWithoutTile(board: Board, tile: BoardTile) {
   const newBoard = { ...board }
-  newBoard.tiles = _.without(newBoard.tiles, tile)
+  newBoard.tiles = _.reject(newBoard.tiles, t => t.id === tile.id)
   return newBoard
 }
 
